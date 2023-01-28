bb1ab39782e983608a8d40924ff33ef95c75013d
Salam4ik <andrej160195@mail.ru>
2023-01-28 20:40:20 +0300

    Predicate

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,9 +12,13 @@ public class Main {
         persons.add(new Person("Коля","Пупкин", 20));
         persons.add(new Person("Витя","Выкрутинский", 27));
         persons.add(new Person("Саша","Петров", 37));
+        persons.add(new Person("Олег", "Петросян", 10));
 
         System.out.println(persons);
-        Collections.sort(persons, new PersonComorator(7));
+        Collections.sort(persons, new PersonComorator(8));
+        System.out.println(persons);
+
+        persons.removeIf(p -> (p.getAge() < 18));
         System.out.println(persons);
     }
 }
